311a65bfe237870935d226a738509431bdc9b61b
santiago966 <santyboal2@gmail.com>
2021-09-08 22:57:55 -0500

    indexxx

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('./config/config');
+require('./server/config/config');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('./public'));
-app.use(require('./routes/index'));
+app.use(require('./server/routes/index'));
 
 app.listen(process.env.PORT, () => {
     console.log(`Conectado al puerto ${process.env.PORT}`);
